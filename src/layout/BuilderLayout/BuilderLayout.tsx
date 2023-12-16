05d5df3d7d2d35b15050112676df1d1af6114a0c
Tarasc33 <tarasc33@gmail.com>
2023-12-16 19:51:40 +0200

    Update builder

--- src/layout/BuilderLayout/BuilderLayout.tsx
+++ src/layout/BuilderLayout/BuilderLayout.tsx
@@ -14,7 +14,7 @@ const BuilderLayout = ({ children }) => {
           <li className="builder-li-main"><Link href="/">Головна</Link></li>
           <li className="builder-li-builder"><Link href="/builder">Конструктор</Link></li>
           <li className="builder-li-profile">
-            <Link href="/builder/settings">{contextValue.authObj.isAuthenticated ? contextValue.authObj.userName : null}</Link>
+            <Link href="/builder/settings">Налаштування</Link>
           </li>
           <li className="builder-li-exit">
             <button onClick={()=> {
@@ -27,7 +27,7 @@ const BuilderLayout = ({ children }) => {
                 token: ''
               }))
               router.push('/')
-            }}>Log out</button>
+            }}>Вихід</button>
           </li>
         </ul>
       </nav>
