99551086165daa06c6a97ec1b2292268f7c944da
Tarasc33 <tarasc33@gmail.com>
2024-01-04 23:02:53 +0200

    update builder

--- src/components/Builder/Builder.js
+++ src/components/Builder/Builder.js
@@ -52,7 +52,7 @@ const StepIndicator = props => (
 
 const Navigation = props => {
   return (
-    <div className="stepper-footer form-btn-block">
+    <div className="stepper-footer.form-btn-block">
       <button
         className="btn-transparent"
         onClick={props.previousStepHandler}
@@ -60,8 +60,7 @@ const Navigation = props => {
       >
         Назад
       </button>
-      <button
-        className="btn stepper-footer-btn btn-continue"
+      <button className="btn stepper-footer-btn btn-continue"
         type="submit"
         disabled={props?.isLastStep ? props.values?.questions?.length === 0 : !props.values?.quizTitle}
         onClick={(e) => (
