905f710f98355f63ed256459789c3435f102ba32
rsvistel <romann.kravets@gmail.com>
2024-01-10 16:09:56 +0200

    add fix

--- src/layout/PrimaryLayout/PrimaryLayout.tsx
+++ src/layout/PrimaryLayout/PrimaryLayout.tsx
@@ -1,9 +1,6 @@
-import PrimaryHeader from "../../components/PrimaryHeader/PrimaryHeader"
-
 const PrimaryLayout = ({ children }) => {
   return (
     <div>
-      {/*<PrimaryHeader/>*/}
       {children}
     </div>
   )
